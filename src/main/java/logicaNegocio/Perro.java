d50a6e60887a47e0b13db4912c98d418b149c14e
Samuel <saks1415@gmail.com>
2024-11-18 10:08:55 -0500

    a la clase perro se le pusieron los metodos de emitir sonido que retorna en ladrido el metodo obtener dieta que retorna en omnivoro esta clase se extiende tambien con animal pero se implementa la clase domestico que retorna en El perro mueve la cola y ladra de felicidad y por ultimo en el metodo tipohabitad retorna en el habitad asignado

--- src/main/java/logicaNegocio/Perro.java
+++ src/main/java/logicaNegocio/Perro.java
@@ -1,5 +1,23 @@
 package logicaNegocio;
 
-public class Perro extends Animal  {
+public class Perro extends Animal implements Domestico {
 
+    private TipoHabitat habitat;
+
+    public Perro() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+
+    public String emitirSonido() {
+        return "Ladrido";
+    }
+    public String obtenerDieta() {
+        return "Omnívoro";
+    }
+    public String interactuarConHumano() {
+        return "El perro mueve la cola y ladra de felicidad";
+    }
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
 }
