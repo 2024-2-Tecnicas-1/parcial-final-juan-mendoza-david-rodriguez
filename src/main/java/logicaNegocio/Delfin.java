9507a70866390b9f0335cae8e466793824db5cc9
elsinnombreprogramador <elprogramadorjuancho@gmail.com>
2024-11-18 10:13:40 -0500

    Se añadieron los metodos abstractos y se completo la clase "Delfin".

--- src/main/java/logicaNegocio/Delfin.java
+++ src/main/java/logicaNegocio/Delfin.java
@@ -1,5 +1,23 @@
 package logicaNegocio;
 
-public class Delfin {
-    // TODO: Aquí va tu código
+public class Delfin extends Animal {
+    
+    private TipoHabitat habitat;
+            
+            public Delfin () {
+                this.habitat = TipoHabitat.ACUATICO;
+            }
+            
+            public String emitirSonido() {
+                return "Chirrido";
+            }
+            
+            public String obtenerDieta() {
+                return "Pescado";
+            }
+            
+            public TipoHabitat getHabitat() {
+                return habitat;
+            }
+    
 }
