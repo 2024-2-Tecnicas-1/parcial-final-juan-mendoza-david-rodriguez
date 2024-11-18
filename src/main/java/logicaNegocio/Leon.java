eb4e0390c54d211598fdd0094ec03c24e493513b
Samuel <saks1415@gmail.com>
2024-11-18 09:51:51 -0500

    en clase tipo habitad se ponen los tipos de habitad que se obtendra en las clases de leon metodos emitir sonido y saber dieta este se extiende en animal

--- src/main/java/logicaNegocio/Leon.java
+++ src/main/java/logicaNegocio/Leon.java
@@ -1,5 +1,22 @@
 package logicaNegocio;
 
-public class Leon {
-    // TODO: Aquí va tu código
+public class Leon extends Animal {
+
+    private TipoHabitat habitat;
+
+    public Leon() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+
+    public String emitirSonido() {
+        return "Rugido";
+    }
+
+    public String obtenerDieta() {
+        return "Carnívoro";
+    }
+
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
 }
